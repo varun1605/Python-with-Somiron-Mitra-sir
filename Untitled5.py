a22738694980ef57f27e3d8e5b98a495c26c397f
varun1605 <80840983+varun1605@users.noreply.github.com>
2021-04-08 21:42:17 +0530

    Add files via upload

---
+++ Untitled5.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+# to print family tree in the below programme
+
+
+# In[96]:
+
+
+grandfather = "sachin"
+grandmother = "durga"
+father =  "prashant"
+mother=  "mithali"
+son =  "subhman"
+daughter =  "sneha"
+wife =  "neha"
+husband = "varun"
+chachi = "sarojini"
+chacha = "bahadur"
+nephew = "pappu"
+niece = "priyanka"
+
+old = f"{grandfather.title()} \n{grandmother.title()}"
+children = f"\n\n\t{son.title()} \n\t{daughter.title()}"
+parents = f"\n\n\t\t{husband.title()} \n\t\t{wife.title()}"
+elder = f"\n\n\t\t\t{father.title()} \n\t\t\t{mother.title()}"
+chachachachi = f"\n\n\t\t\t\t{chacha.title()} \n\t\t\t\t{chachi.title()}"
+bhanjabhanji = f"\n\n\n\t\t\t\t\t{nephew.title()} \n\t\t\t\t\t{niece.title()}"
+
+
+print(old)
+
+print(children)
+
+print(parents)
+
+print(elder)
+
+print(chachachachi)
+
+print(bhanjabhanji)
+
+print("\n\n\t\t\t\t\tAll are members of a family")
+
+
+
+
+# In[13]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
