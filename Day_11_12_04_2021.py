b07b0386c5e22be9b3429793a2d401f0e1e14a05
varun1605 <80840983+varun1605@users.noreply.github.com>
2021-04-13 20:42:49 +0530

    Add files via upload

---
+++ Day_11_12_04_2021.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# to add an element in the list
+
+
+# In[3]:
+
+
+team = ["virat","kl rahul","dhoni","rohitsharma"]
+
+# to add any name/element in the list then we need to use the command known as "append"
+
+team.append("shikhar dhawan")
+
+print(team)
+        
+
+
+# In[4]:
+
+
+# now trying to make the first letters of the names as capital/ upper case
+
+
+# In[5]:
+
+
+team = ["virat.title()","kl rahul","dhoni","rohitsharma"]
+
+print(team)
+
+
+# In[6]:
+
+
+#so we have obesrved that when we try to give the command in between the quotes then it print it as it is, so lets try some different methods to get our job done
+
+
+# In[7]:
+
+
+team = ["virat".title(),"kl rahul"title(),"dhoni"title(),"rohitsharma"title()]
+
+print(team)
+
+
+# In[8]:
+
+
+team = ["virat".title(),"kl rahul"title(),"dhoni"title(),"rohitsharma"title()]
+
+print(team)
+
+
+# In[11]:
+
+
+
+team = ["virat".title(),"kl rahul".title(),"dhoni","rohitsharma"]
+
+print(team)
+
+
+# In[12]:
+
+
+# so this is the proof that when we give a command outside the quotes then the command is treated as a command and our job is done.
+
+
+# In[ ]:
+
+
+# simillarly we can also do all the characters as upper case by using the respective command which is ".upper()"
+
+
+# In[13]:
+
+
+team = ["virat".upper(),"kl rahul".upper(),"dhoni".upper(),"rohitsharma".upper()]
+
+print(team)
+
+
+# In[14]:
+
+
+prin(team)
+
+
+# In[18]:
+
+
+team = ["virat".title(),"kl rahul","dhoni","rohitsharma"]
+print(team)
+
+
+# In[19]:
+
+
+team = ["virat".title(),"kl rahul","dhoni","rohitsharma"]
+print(team)
+
+
+# In[20]:
+
+
+# now to check the numberings
+
+
+# In[27]:
+
+
+team = ["virat","kl rahul","dhoni","rohitsharma"]
+
+team("lis
+
+
+# In[28]:
+
+
+team = ["virat","kl rahul","dhoni","rohitsharma"]
+
+team.append("varun"[2])
+
+print(team)
+
+
+# In[31]:
+
+
+team = ["virat","kl rahul","dhoni","rohitsharma"]
+team.append("sam"[1])
+print(team)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
